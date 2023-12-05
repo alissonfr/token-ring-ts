981a4106ffd511fb0071b16dd5828b05605486a1
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-12-04 22:40:54 -0300

    feat: websocket lib

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,6 @@
-console.log("servidor iniciado")
+import { Socket } from "./Socket";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const webSocketServer = new Socket(Number(process.env.SERVER_PORT));
